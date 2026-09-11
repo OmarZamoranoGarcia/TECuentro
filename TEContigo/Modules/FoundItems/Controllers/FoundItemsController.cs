f27a95ce157cee859c1de4a2fe21e70b570d6597
OmarZamoranoGarcia <omarchucho13Zago0413@hotmail.com>
2026-09-10 19:02:10 -0700

    Finish: FoundItems Module

---
+++ TEContigo/Modules/FoundItems/Controllers/FoundItemsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TEContigo.Modules.FoundItems.DTOs;
+using TEContigo.Modules.FoundItems.Services;
+
+namespace TEContigo.Modules.FoundItems.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class FoundItemsController : ControllerBase
+{
+    private readonly IFoundItemsService _foundItemsService;
+
+    public FoundItemsController(
+        IFoundItemsService foundItemsService)
+    {
+        _foundItemsService = foundItemsService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetFoundItems()
+    {
+        var foundItems =
+            await _foundItemsService.GetAllAsync();
+
+        return Ok(foundItems);
+    }
+
+    [HttpGet("{id:long}")]
+    public async Task<IActionResult> GetFoundItem(long id)
+    {
+        var foundItem =
+            await _foundItemsService.GetByIdAsync(id);
+
+        return Ok(foundItem);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateFoundItem(
+        CreateFoundItemDto dto)
+    {
+        var response =
+            await _foundItemsService.CreateAsync(dto);
+
+        return StatusCode(
+            StatusCodes.Status201Created,
+            response);
+    }
+
+    [HttpPut("{id:long}")]
+    public async Task<IActionResult> UpdateFoundItem(
+        long id,
+        UpdateFoundItemDto dto)
+    {
+        var response =
+            await _foundItemsService.UpdateAsync(id, dto);
+
+        return Ok(response);
+    }
+
+    [HttpDelete("{id:long}")]
+    public async Task<IActionResult> DeleteFoundItem(long id)
+    {
+        var response =
+            await _foundItemsService.DeleteAsync(id);
+
+        return Ok(response);
+    }
+}
---
+++ TEContigo/Modules/FoundItems/DTOs/FoundItemResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TEContigo.Modules.FoundItems.DTOs
+{
+    public class FoundItemResponseDto
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public long? Id { get; set; }
+    }
+}

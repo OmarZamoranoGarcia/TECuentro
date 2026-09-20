ff2b56c7e00dd50cbdd5caeda9c337ea38a69079
OmarZamoranoGarcia <omarchucho13Zago0413@hotmail.com>
2026-09-20 16:26:15 -0700

    Add RegisterModeratorDto

---
+++ TEContigo/Modules/Auth/DTOs/RegisterModeratorDto.cs
@@ -0,0 +1,19 @@
+﻿namespace TEContigo.Modules.Auth.DTOs
+{
+    public class RegisterModeratorDto
+    {
+        public long ControlNumber { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastNamePaternal { get; set; } = string.Empty;
+
+        public string LastNameMaternal { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+     
+        public string Role { get; set; } = string.Empty;
+    }
+}

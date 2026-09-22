a3d6bcef4c160cbbbc32b37fb157adbe73029e18
OmarZamoranoGarcia <omarchucho13Zago0413@hotmail.com>
2026-09-21 18:45:29 -0700

    DTOs for pagination in GetAllAsync Methods

---
+++ TEContigo/Shared/Pagination/PaginationQueryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TEContigo.Shared.Pagination
+{
+    public class PaginationQueryDto
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
